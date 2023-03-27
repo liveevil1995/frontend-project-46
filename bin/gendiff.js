bd2fdf5c294d29b7dbf91e68fed594a15cd7683e
Ilya <shining1234@yandex.ru>
2023-03-27 17:22:49 +0300

    adding format option and usage

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,9 @@ const program = new Command();
 
 program
   .name('gendiff')
+  .usage("[options] <filepath1> <filepath2>")
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
