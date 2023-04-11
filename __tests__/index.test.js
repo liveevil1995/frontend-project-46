00e7949da0f4fbf73f0c06137ea7aa2b2ffa673a
Ilya <shining1234@yandex.ru>
2023-04-11 14:09:17 +0300

    Changing the test to test nested structures. Implemented fixture with nesting. Stylish formatter implemented

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import { readFileSync } from 'fs';
-import gendiff from '../src/index.js';
+import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,8 +11,8 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const filepath1 = getFixturePath('file1.json');
 const filepath2 = getFixturePath('file2.json');
 
-const stylish = readFileSync(path.resolve(process.cwd(), '__fixtures__/result.txt'), 'utf-8');
+const result = readFileSync(path.resolve(process.cwd(), '__fixtures__/result.txt'), 'utf-8');
 
 test('Examination JSON', () => {
-  expect(gendiff(filepath1, filepath2)).toEqual(stylish);
+  expect(genDiff(filepath1, filepath2)).toEqual(result);
 });
--- src/index.js
+++ src/index.js
@@ -2,48 +2,14 @@ import _ from 'lodash';
 import * as fs from 'node:fs';
 import path from 'node:path';
 import { cwd } from 'process';
-import formatter from './formatter.js';
 import parser from './parser.js';
+import stylish from './formattersFiles/stylish.js';
+import diffFunc from './difFunc.js';
 
-const gendiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const obj1 = parser(fs.readFileSync(path.resolve(`${cwd()}`, filepath1), 'utf8'), path.extname(filepath1).slice(1));
   const obj2 = parser(fs.readFileSync(path.resolve(`${cwd()}`, filepath2), 'utf8'), path.extname(filepath2).slice(1));
-  const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
-  const diff = keys.map((key) => {
-    if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
-      return {
-        type: 'deleted',
-        key,
-        value: obj1[key],
-      };
-    }
-
-    if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
-      return {
-        type: 'added',
-        key,
-        value: obj2[key],
-      };
-    }
-
-    if (obj1[key] !== obj2[key]) {
-      return {
-        type: 'changed',
-        key,
-        value: obj1[key],
-        value2: obj2[key],
-      };
-    }
-
-    if (obj1[key] === obj2[key]) {
-      return {
-        type: 'unchanged',
-        key,
-        value: obj1[key],
-      };
-    }
-  });
-  return formatter(diff);
+  return stylish(diffFunc(obj1, obj2), format);
 };
 
-export default gendiff;
+export default genDiff;
