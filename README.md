6b9401158ff8c6ca840755a9cb44e1a9ac69e56a
Ilya <shining1234@yandex.ru>
2023-04-18 11:48:27 +0300

    Changing the design of testing to a more successful one. Filling out the profile description

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 [![Node CI](https://github.com/liveevil1995/frontend-project-46/actions/workflows/main.yml/badge.svg)](https://github.com/liveevil1995/frontend-project-46/actions/workflows/main.yml)
 [![Maintainability](https://api.codeclimate.com/v1/badges/7bf6812dcd5701554d0a/maintainability)](https://codeclimate.com/github/liveevil1995/frontend-project-46/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/7bf6812dcd5701554d0a/test_coverage)](https://codeclimate.com/github/liveevil1995/frontend-project-46/test_coverage)
+
+# :computer: Difference Calculator :
+A difference calculator is a program that determines the difference between two data structures. A similar mechanism is used when outputting tests or when automatically tracking changes in configuration files.
+
+## :wrench: Install
+
+1. Clone the repository with the command: `git clone`.
+2. Install the program with the command: `npm install`.
+3. Program commands: `-f format`, `-h help`.
+4. Formatting styles: `plain, json, stylish`.
+
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,19 +6,17 @@ import genDiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filepath) => readFileSync(getFixturePath(filepath), 'utf-8').trim();
 
-const filepath1 = getFixturePath('file1.json');
-const filepath2 = getFixturePath('file2.json');
-const filepath1yml = getFixturePath('file1.yml');
-const filepath2yml = getFixturePath('file2.yml');
-
-const result = readFileSync(path.resolve(process.cwd(), '__fixtures__/result.txt'), 'utf-8');
-
-test('Examination JSON', () => {
-  expect(genDiff(filepath1, filepath2)).toEqual(result);
-  expect(genDiff(filepath1yml, filepath2yml)).toEqual(result);
-});
-
-test('Examination YML', () => {
-  expect(genDiff(filepath1yml, filepath2yml)).toEqual(result);
+test.each([
+  ['file1.json', 'file2.json', 'stylish', 'result-stylish.txt'],
+  ['file1.yml', 'file2.yml', 'stylish', 'result-stylish.txt'],
+  ['file1.json', 'file2.json', 'plain', 'result-plain.txt'],
+  ['file1.yml', 'file2.yml', 'plain', 'result-plain.txt'],
+  ['file1.json', 'file2.json', 'json', 'result-json.txt'],
+  ['file1.yml', 'file2.yml', 'json', 'result-json.txt'],
+  ['file1.json', 'file2.json', undefined, 'result-stylish.txt'],
+])('Examination genDiff', (file1, file2, format, resultFile) => {
+  const actual = genDiff(getFixturePath(file1), getFixturePath(file2), format);
+  const result = readFile(resultFile); expect(actual).toBe(result);
 });
