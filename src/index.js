e3a272515ca72bab5b5f7b39b773b4c026980b3f
Ilya <shining1234@yandex.ru>
2023-04-20 16:28:38 +0300

    creating getFileData and getExtensionFile functions

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import parser from './parser.js';
 import getFormattedContent from './formattersFiles/index.js';
 import buildDiffTree from './buildDiffTree.js';
 
+const getFileData = (filepath) => fs.readFileSync(path.resolve(`${cwd()}`, filepath), 'utf8');
+const getExtensionFile = (filepath) => path.extname(filepath).slice(1);
+
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const obj1 = parser(fs.readFileSync(path.resolve(`${cwd()}`, filepath1), 'utf8'), path.extname(filepath1).slice(1));
-  const obj2 = parser(fs.readFileSync(path.resolve(`${cwd()}`, filepath2), 'utf8'), path.extname(filepath2).slice(1));
+  const obj1 = parser(getFileData(filepath1), getExtensionFile(filepath1));
+  const obj2 = parser(getFileData(filepath2), getExtensionFile(filepath2));
   return getFormattedContent(buildDiffTree(obj1, obj2), format);
 };
 
