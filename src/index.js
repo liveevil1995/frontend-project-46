a300098476fe08c7dc619054d9f59e44fe7a8516
Ilya <shining1234@yandex.ru>
2023-04-20 16:12:48 +0300

    deleting unnecessary folders and files. Renaming a function

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import * as fs from 'node:fs';
 import path from 'node:path';
 import { cwd } from 'process';
 import parser from './parser.js';
-import formattersFunc from './formattersFiles/index.js';
+import getFormattedContent from './formattersFiles/index.js';
 import buildDiffTree from './buildDiffTree.js';
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const obj1 = parser(fs.readFileSync(path.resolve(`${cwd()}`, filepath1), 'utf8'), path.extname(filepath1).slice(1));
   const obj2 = parser(fs.readFileSync(path.resolve(`${cwd()}`, filepath2), 'utf8'), path.extname(filepath2).slice(1));
-  return formattersFunc(buildDiffTree(obj1, obj2), format);
+  return getFormattedContent(buildDiffTree(obj1, obj2), format);
 };
 
 export default genDiff;
